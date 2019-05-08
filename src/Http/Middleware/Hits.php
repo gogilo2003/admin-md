910ca94677cb08e142283717adcf3171c6a87eeb
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2019-05-08 17:46:56 +0300

    Updates

--- src/Http/Middleware/Hits.php
+++ src/Http/Middleware/Hits.php
@@ -19,23 +19,24 @@ class Hits
     {
 
         // dd($request);
+        if (!is_admin_path()) {
+            $hit = new Hit;
 
-        $hit = new Hit;
+            $hit->ip = $request->ip();
+            $hit->url = $request->fullUrl();
+            $hit->method = $request->method();
+            $hit->user_agent = $request->server('HTTP_USER_AGENT');
+            $hit->browser = getBrowser($request->server('HTTP_USER_AGENT'));
+            $hit->platform = getOs($request->server('HTTP_USER_AGENT'));
+            $hit->language = $request->server('HTTP_ACCEPT_LANGUAGE') ? $request->server('HTTP_ACCEPT_LANGUAGE') : 'Unknown';
+            $hit->wants_json = $request->wantsJson();
+            $hit->is_json = $request->isJson();
+            $hit->ajax = $request->ajax();
+            $hit->pjax = $request->pjax();
 
-        $hit->ip = $request->ip();
-        $hit->url = $request->fullUrl();
-        $hit->method = $request->method();
-        $hit->user_agent = $request->server('HTTP_USER_AGENT');
-        $hit->browser = getBrowser($request->server('HTTP_USER_AGENT'));
-        $hit->platform = getOs($request->server('HTTP_USER_AGENT'));
-        $hit->language = $request->server('HTTP_ACCEPT_LANGUAGE') ? $request->server('HTTP_ACCEPT_LANGUAGE') : 'Unknown';
-        $hit->wants_json = $request->wantsJson();
-        $hit->is_json = $request->isJson();
-        $hit->ajax = $request->ajax();
-        $hit->pjax = $request->pjax();
-
-        $hit->save();
-
+            $hit->save();
+        }
+        
         return $next($request);
     }
 
