c9c6bf3bb98229ece2515b54b7a0e9ae4de2b772
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2019-05-19 08:17:24 +0300

    Added image cropping capability on picture uploads

--- resources/views/pictures/add.blade.php
+++ resources/views/pictures/add.blade.php
@@ -29,7 +29,7 @@
 	<hr>
 	<form method="post" action="{{route('admin-pictures-add')}}" role="form" accept-charset="UTF-8" enctype="multipart/form-data">
 		<div class="row">
-			<div class="col-sm-6 col-md-4 col-lg-4">
+			<div class="col-sm-12 col-md-6 col-lg-6">
 				<div class="form-group{!! $errors->has('name') ? ' has-error':'' !!}">
 					<label for="name">Picture</label>
 					<div>
@@ -49,35 +49,35 @@
 					{!! $errors->has('name') ? '<span class="text-danger">'.$errors->first('name').'</span>' : ''!!}
 				</div>
 			</div>
-			<div class="col-sm-6 col-md-8 col-lg-8">
+			<div class="col-sm-12 col-md-6 col-lg-6">
 
 				<div class="form-group{!! $errors->has('title') ? ' has-error':'' !!}">
 					<label for="title">Title</label>
-					<input type="text" class="form-control" id="title" name="title" placeholder="Enter title"{!! ((old('title')) ? ' value="'.old('title').'"' : '') !!}>
+					<input type="text" class="form-control" id="title" name="title" {!! ((old('title')) ? ' value="'.old('title').'"' : '') !!}>
 					{!! $errors->has('title') ? '<span class="text-danger">'.$errors->first('title').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('alt') ? ' has-error':'' !!}">
 					<label for="alt">Alt Text</label>
-					<input type="text" class="form-control" id="alt" name="alt" placeholder="Enter alt text"{!! ((old('alt')) ? ' value="'.old('alt').'"' : '') !!}>
+					<input type="text" class="form-control" id="alt" name="alt" {!! ((old('alt')) ? ' value="'.old('alt').'"' : '') !!}>
 					{!! $errors->has('alt') ? '<span class="text-danger">'.$errors->first('alt').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('caption') ? ' has-error':'' !!}">
 					<label for="caption">Caption</label>
-					<input type="text" class="form-control" id="caption" name="caption" placeholder="Enter caption"{!! ((old('caption')) ? ' value="'.old('caption').'"' : '') !!}>
+					<input type="text" class="form-control" id="caption" name="caption" {!! ((old('caption')) ? ' value="'.old('caption').'"' : '') !!}>
 					{!! $errors->has('caption') ? '<span class="text-danger">'.$errors->first('caption').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('url') ? ' has-error':'' !!}">
 					<label for="url">Url</label>
-					<input type="text" class="form-control" id="url" name="url" placeholder="Enter url e.g. https://www.example.com"{!! ((old('url')) ? ' value="'.old('url').'"' : '') !!}>
+					<input type="text" class="form-control" id="url" name="url"{!! ((old('url')) ? ' value="'.old('url').'"' : '') !!}>
 					{!! $errors->has('url') ? '<span class="text-danger">'.$errors->first('url').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group">
 					<label for="picture_category">Picture Category</label>
-					<select class="selectpicker form-control" id="picture_category" name="picture_category">
+					<select data-style="btn btn-link" {{ ($categories->count() > 5) ? 'data-live-search="true" data-size="5"' : '' }} class="selectpicker form-control" id="picture_category" name="picture_category">
 						@foreach ($categories as $category)
 							<option value="{{ $category->id }}">{{ $category->title }}</option>
 						@endforeach
--- resources/views/pictures/edit.blade.php
+++ resources/views/pictures/edit.blade.php
@@ -30,9 +30,9 @@
 	<hr>
 	<form method="post" action="{{route('admin-pictures-edit-post')}}" role="form" accept-charset="UTF-8" enctype="multipart/form-data">
 		<div class="row">
-			<div class="col-sm-6 col-md-4 col-lg-4">
+			<div class="col-sm-12 col-md-6 col-lg-6">
 				<div class="form-group{!! $errors->has('name') ? ' has-error':'' !!}">
-					<label for="name">Picture</label>
+					<label class="bmd-label-static" for="name">Picture</label>
 					<div>
 						<img id="image_preview" src="{{ url('public/images/pictures/'.$picture->name) }}" class="img-responsive img-fluid" alt="{{ $picture->title }}">
 					</div>
@@ -50,35 +50,35 @@
 					{!! $errors->has('name') ? '<span class="text-danger">'.$errors->first('name').'</span>' : ''!!}
 				</div>
 			</div>
-			<div class="col-sm-6 col-md-8 col-lg-8">
+			<div class="col-sm-12 col-md-6 col-lg-6">
 
 				<div class="form-group{!! $errors->has('title') ? ' has-error':'' !!}">
-					<label for="title">Title</label>
-					<input type="text" class="form-control" id="title" name="title" placeholder="Enter title"{!! ((old('title')) ? ' value="'.old('title').'"' : ' value="'.$picture->title.'"') !!}>
+					<label class="bmd-label-static" for="title">Title</label>
+					<input type="text" class="form-control" id="title" name="title"{!! ((old('title')) ? ' value="'.old('title').'"' : ' value="'.$picture->title.'"') !!}>
 					{!! $errors->has('title') ? '<span class="text-danger">'.$errors->first('title').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('alt') ? ' has-error':'' !!}">
-					<label for="alt">Alt Text</label>
-					<input type="text" class="form-control" id="alt" name="alt" placeholder="Enter alt text"{!! ((old('alt')) ? ' value="'.old('alt').'"' : ' value="'.$picture->alt.'"') !!}>
+					<label class="bmd-label-static" for="alt">Alt Text</label>
+					<input type="text" class="form-control" id="alt" name="alt"{!! ((old('alt')) ? ' value="'.old('alt').'"' : ' value="'.$picture->alt.'"') !!}>
 					{!! $errors->has('alt') ? '<span class="text-danger">'.$errors->first('alt').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('caption') ? ' has-error':'' !!}">
-					<label for="caption">Caption</label>
-					<input type="text" class="form-control" id="caption" name="caption" placeholder="Enter caption"{!! ((old('caption')) ? ' value="'.old('caption').'"' : ' value="'.$picture->caption.'"') !!}>
+					<label class="bmd-label-static" for="caption">Caption</label>
+					<input type="text" class="form-control" id="caption" name="caption"{!! ((old('caption')) ? ' value="'.old('caption').'"' : ' value="'.$picture->caption.'"') !!}>
 					{!! $errors->has('caption') ? '<span class="text-danger">'.$errors->first('caption').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group{!! $errors->has('url') ? ' has-error':'' !!}">
-					<label for="url">Url</label>
-					<input type="text" class="form-control" id="url" name="url" placeholder="Enter url e.g. https://www.example.com"{!! ((old('url')) ? ' value="'.old('url').'"' : ' value="'.$picture->url.'"') !!}>
+					<label class="bmd-label-static" for="url">Url</label>
+					<input type="text" class="form-control" id="url" name="url"{!! ((old('url')) ? ' value="'.old('url').'"' : ' value="'.$picture->url.'"') !!}>
 					{!! $errors->has('url') ? '<span class="text-danger">'.$errors->first('url').'</span>' : '' !!}
 				</div>
 
 				<div class="form-group">
-					<label for="picture_category">Picture Category</label>
-					<select class="selectpicker form-control" id="picture_category" name="picture_category">
+					<label class="bmd-label-static" for="picture_category">Picture Category</label>
+					<select data-style="btn btn-link" {{ ($categories->count() > 5) ? 'data-live-search="true" data-size="5"' : '' }} class="selectpicker form-control" id="picture_category" name="picture_category">
 						@foreach ($categories as $category)
 							<option value="{{ $category->id }}" {{ ($picture->category->id == $category->id) ? 'selected' : '' }}>{{ $category->title }}</option>
 						@endforeach
