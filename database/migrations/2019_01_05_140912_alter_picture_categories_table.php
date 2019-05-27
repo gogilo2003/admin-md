9538b8ab04e6b9ae548f400658c4144dc73a6265
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2019-05-27 06:53:27 +0300

    Just incase

---
+++ database/migrations/2019_01_05_140912_alter_picture_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterPictureCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pricture_categories', function (Blueprint $table) {
+            $table->integer('max_width')->default(null)->nullable()->change();
+            $table->integer('max_height')->default(null)->nullable()->change();
+            $table->integer('min_width')->default(null)->nullable()->change();
+            $table->integer('min_height')->default(null)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pricture_categories', function (Blueprint $table) {
+            //
+        });
+    }
+}
