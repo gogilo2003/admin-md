d9f7a73bbeacb8473922fe9863042b8bf6e5e2bd
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2019-09-26 03:51:56 +0300

    Fixed settings saving bug

--- src/Http/Controllers/SettingsController.php
+++ src/Http/Controllers/SettingsController.php
@@ -34,22 +34,42 @@ class SettingsController extends Controller
 
     public function postSettings(Request $request)
     {
-        $menu = config('admin.menu');
-        $admin = array(
-            'articles' => $request->has_articles ? true : false,
-            'pictures' => $request->has_pictures ? true : false,
-            'videos' => $request->has_videos ? true : false,
-            'files' => $request->has_files ? true : false,
-            'projects' => $request->has_projects ? true : false,
-            'profiles' => $request->has_profiles ? true : false,
-            'sermons' => $request->has_sermons ? true : false,
-            'events' => $request->has_events ? true : false,
-            'packages' => $request->has_packages ? true : false,
-            'products' => $request->has_products ? true : false,
-            'contact' => $request->email,
-            'content_css' => $request->content_css,
-            'menu' => $menu,
-        );
+        // $menu = config('admin.menu');
+        // $admin = array(
+        //     'articles' => $request->has_articles ? true : false,
+        //     'pictures' => $request->has_pictures ? true : false,
+        //     'videos' => $request->has_videos ? true : false,
+        //     'files' => $request->has_files ? true : false,
+        //     'projects' => $request->has_projects ? true : false,
+        //     'profiles' => $request->has_profiles ? true : false,
+        //     'sermons' => $request->has_sermons ? true : false,
+        //     'events' => $request->has_events ? true : false,
+        //     'packages' => $request->has_packages ? true : false,
+        //     'products' => $request->has_products ? true : false,
+        //     'contact' => $request->email,
+        //     'content_css' => $request->content_css,
+        //     'menu' => $menu,
+        // );
+
+        $admin = config('admin');
+        // dump($admin);
+
+        config(['admin.articles'=>$request->has_articles ? true : false]);
+        config(['pictures' => $request->has_pictures ? true : false]);
+        config(['videos' => $request->has_videos ? true : false]);
+        config(['files' => $request->has_files ? true : false]);
+        config(['projects' => $request->has_projects ? true : false]);
+        config(['profiles' => $request->has_profiles ? true : false]);
+        config(['sermons' => $request->has_sermons ? true : false]);
+        config(['events' => $request->has_events ? true : false]);
+        config(['packages' => $request->has_packages ? true : false]);
+        config(['products' => $request->has_products ? true : false]);
+        config(['contact' => $request->email]);
+        config(['content_css' => $request->content_css]);
+
+        $admin = config('admin');
+        // dd($admin);
+
 
         \save_admin_config($admin);
 
