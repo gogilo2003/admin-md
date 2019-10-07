951e2fc5234dce8cbc9e585fc6e62e57a0002f2b
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2019-10-08 02:03:29 +0300

    Changed picture page relationship

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -41,7 +41,7 @@ class Page extends Model
 
     public function getPicturesAttribute()
     {
-        $pictures = Picture::with('category')->where('published',1)->whereIn('id',$this->picture_categories->pluck('id')->toArray());
+        $pictures = Picture::with('category')->where('published',1)->whereIn('id',$this->picture_categories->pluck('id')->toArray())->get();
         return $pictures;
     }
     
