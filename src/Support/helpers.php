179f9b40f084b798d06fb079a0238206e61654ad
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-02-01 06:08:49 +0300

    modified str_slug_unique to remove any slashes in the resulting slug

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1292,7 +1292,8 @@ function save_config($key,$value){
 }
 
 function str_slug_unique($table,$field,$value,$id=null){
-	$name = str_slug(strip_tags($value));
+    $name = str_replace('/',' ',strip_tags($value));
+	// $name = str_slug(strip_tags($value));
 
 	if ($id) {
 		$item = DB::table($table)->where('id','=',$id)->where($field,'LIKE',$name.'%')->first();
