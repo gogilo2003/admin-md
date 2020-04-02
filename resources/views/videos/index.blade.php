36f8238c24c984c6599b600c40b302c90aeac384
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-04-02 06:55:13 +0300

    Added publish/unpublish feature for video management

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -27,7 +27,7 @@
 	<hr>
 	<div class="row">
 		@foreach ($videos as $video)
-			<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+			<div class="col-xs-12 col-sm-6 col-md-4">
 				<div class="thumbnail text-center">
 					<i class="fa fa-file-video-o fa-5x"></i><br>
 					<div style="height:180px; margin: 15px auto; overflow-y: hidden;">
@@ -38,7 +38,7 @@
 					
 					<p>
 						<a href="{{route('admin-videos-edit',$video->id)}}" class="btn btn-outline-primary btn-sm rounded-pill"><span class="fa fa-edit"></span>&nbsp;&nbsp; Edit</a>
-						<button id="publishVideoButton" class="btn btn-sm btn-outline-success rounded-pill" data-id="{{ $video->id }}"><i class="material-icons">{{ $video->published ? 'get_app' : 'publish' }}</i> <span>{{ $video->published ? 'Un-publish' : 'Publish' }}</span></button>
+						<button class="publishVideoButton btn btn-sm btn-outline-success rounded-pill" data-id="{{ $video->id }}"><i class="material-icons">{{ $video->published ? 'get_app' : 'publish' }}</i> <span>{{ $video->published ? 'Un-publish' : 'Publish' }}</span></button>
 					</p>
 				</div>
 			</div>
@@ -59,21 +59,23 @@
 
 @push('scripts_bottom')
 	<script type="text/javascript">
-		document.getElementById('publishVideoButton').addEventListener('click', function(e){
-			let btn = this
-			let url = '{{ route('api-admin-videos-publish') }}'
-			let data = {
-				id: this.getAttribute('data-id'),
-				api_token: '@api_token'
-			}
-			$.post(url,data).then(function(response){
-				if (response.success) {
-					let icon = btn.querySelector('i')
-					icon.innerHTML = response.video.published ? 'get_app' : 'publish'
-					let caption = btn.querySelector('span')
-					caption.innerHTML = response.video.published ? 'Un-publish' : 'Publish'
-					$.notify({message:response.message,icon:'done'},{type: 'success'})
+		document.querySelectorAll('button.publishVideoButton').forEach(function(item){
+			item.addEventListener('click', function(e){
+				let btn = this
+				let url = '{{ route('api-admin-videos-publish') }}'
+				let data = {
+					id: this.getAttribute('data-id'),
+					api_token: '@api_token'
 				}
+				$.post(url,data).then(function(response){
+					if (response.success) {
+						let icon = btn.querySelector('i')
+						icon.innerHTML = response.video.published ? 'get_app' : 'publish'
+						let caption = btn.querySelector('span')
+						caption.innerHTML = response.video.published ? 'Un-publish' : 'Publish'
+						$.notify({message:response.message,icon:'done'},{type: 'success'})
+					}
+				})
 			})
 		})
 	</script>
