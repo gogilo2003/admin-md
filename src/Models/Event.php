8681c90a896b40fd36dc2f469f488b4327f41af6
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-05-11 11:48:52 +0300

    Added date casting for held_at field in the Events Model

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -9,18 +9,19 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
 
-	protected $appends = ['pages'];
-	
+    protected $appends = ['pages'];
+    protected $dates = ['held_at'];
+
 	public function link()
 	{
 		return $this->morphOne('Ogilo\AdminMd\Models\Link','linkable');
 	}
-	
+
 	public function page()
 	{
 		return $this->belongsTo('Ogilo\AdminMd\Models\Page');
 	}
-	
+
 	public function admins()
 	{
 		return $this->belongsToMany('Ogilo\AdminMd\Models\Admin');
