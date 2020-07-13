29e0a6ce10803b2e6985bcd07a990dac6c609651
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-07-13 12:22:41 +0300

    Added Table existance checking

--- database/migrations/2020_06_20_140930_create_event_schedules_table.php
+++ database/migrations/2020_06_20_140930_create_event_schedules_table.php
@@ -13,19 +13,21 @@ class CreateEventSchedulesTable extends Migration
      */
     public function up()
     {
-        Schema::create('event_schedules', function (Blueprint $table) {
-            Schema::disableForeignKeyConstraints();
-            $table->increments('id');
-            $table->time('start_at');
-            $table->time('end_at');
-            $table->string('title');
-            $table->text('content')->nullable();
-            $table->bigInteger('event_day_id')->unsigned();
-            $table->foreign('event_day_id')->references('id')->on('event_days');
-            $table->boolean('published')->default(false);
-            $table->timestamps();
-            Schema::enableForeignKeyConstraints();
-        });
+        if(!Schema::hasTable('event_schedules')){
+            Schema::create('event_schedules', function (Blueprint $table) {
+                Schema::disableForeignKeyConstraints();
+                $table->increments('id');
+                $table->time('start_at');
+                $table->time('end_at');
+                $table->string('title');
+                $table->text('content')->nullable();
+                $table->bigInteger('event_day_id')->unsigned();
+                $table->foreign('event_day_id')->references('id')->on('event_days');
+                $table->boolean('published')->default(false);
+                $table->timestamps();
+                Schema::enableForeignKeyConstraints();
+            });
+        }
     }
 
     /**
