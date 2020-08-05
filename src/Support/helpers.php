57f34dd3630c661ada6d5f0c05c1588f1603831e
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2020-08-05 05:59:59 +0300

    Changed keywords function

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1568,7 +1568,25 @@ if(!function_exists('stop_words')){
 if (!function_exists('keywords')) {
 	function keywords($text)
 	{
-		return implode(', ', stop_words($text));
+		$min_word_length = 3;
+        $avoid = ['the','to','i','am','is','are','he','she','a','an','and','here','there','can','could','were','has','have','had','been','welcome','of','home','&nbsp;','&ldquo;','words','into','this','there'];
+        $strip_arr = ["," ,"." ,";" ,":", "\"", "'", "“","”","(",")", "!","?"];
+
+        $str_clean = str_replace( $strip_arr, "", $str);
+        $str_arr = explode(' ', $str_clean);
+        $clean_arr = [];
+        foreach($str_arr as $word)
+        {
+            if(strlen($word) > $min_word_length)
+            {
+                $word = strtolower($word);
+                if(!in_array($word, $avoid)) {
+                    $clean_arr[] = $word;
+                }
+            }
+        }
+
+        return implode(',', $clean_arr);
 	}
 }
 
