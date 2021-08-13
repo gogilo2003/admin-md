09bd485351e63924af03c3799b7b28bac2c2c721
Ogilo G.D. Ouma <gogilo2003@hotmail.com>
2021-08-13 04:59:54 +0300

    Added Blogs admin views

---
+++ resources/views/blogs/sidebar.blade.php
@@ -0,0 +1,4 @@
+<li class="nav-item{{ is_current_path('admin-article_categories') ? ' active' : ''}}"><a href="{{ route('admin-article_categories') }}" class="nav-link"><i class="fa fa-list-alt"></i> Article Categories</a></li>
+<li class="nav-item{{ is_current_path('admin-article_categories-add') ? ' active' : ''}}"><a href="{{ route('admin-article_categories-add') }}" class="nav-link"><i class="fa fa-plus"></i> Create article Category</a></li>
+<li class="nav-item{{ is_current_path('admin-articles') ? ' active' : ''}}"><a href="{{ route('admin-articles') }}" class="nav-link"><i class="fa fa-files-o"></i> Articles</a></li>
+<li class="nav-item{{ is_current_path('admin-articles-add') ? ' active' : ''}}"><a href="{{ route('admin-articles-add') }}" class="nav-link"><i class="fa fa-plus"></i> Create Article</a></li>
