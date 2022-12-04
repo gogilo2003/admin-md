c6d673d80fc6267b23fefbe2b28337553d58e5f1
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-12-04 03:36:34 +0300

    Updated loader

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,13 @@
 @section('content')
     <div class="loader-wrap">
         <div class="loader">
-            <img src="{{ asset('images/loader.gif') }}" alt="">
+            {{-- <img src="{{ asset('vendor/admin/images/loader.gif') }}" alt=""> --}}
+            <div class="loadingio-eclipse">
+                <div class="ldio-rpinwye8j0b">
+                    <div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="row" style="min-height: 50vh; display: flex; justify-content: center; align-items: center">
@@ -75,6 +81,53 @@
 
         .loader-wrap .loader img {
             width: 100%;
+        }
+
+        @keyframes ldio-rpinwye8j0b {
+            0% {
+                transform: rotate(0deg)
+            }
+
+            50% {
+                transform: rotate(180deg)
+            }
+
+            100% {
+                transform: rotate(360deg)
+            }
+        }
+
+        .ldio-rpinwye8j0b div {
+            position: absolute;
+            animation: ldio-rpinwye8j0b 1s linear infinite;
+            width: 160px;
+            height: 160px;
+            top: 20px;
+            left: 20px;
+            border-radius: 50%;
+            box-shadow: 0 4px 0 0 #4eff03;
+            transform-origin: 80px 82px;
+        }
+
+        .loadingio-eclipse {
+            width: 200px;
+            height: 200px;
+            display: inline-block;
+            overflow: hidden;
+        }
+
+        .ldio-rpinwye8j0b {
+            width: 100%;
+            height: 100%;
+            position: relative;
+            transform: translateZ(0) scale(1);
+            backface-visibility: hidden;
+            transform-origin: 0 0;
+            /* see note above */
+        }
+
+        .ldio-rpinwye8j0b div {
+            box-sizing: content-box;
         }
     </style>
 @stop
