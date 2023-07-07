acccb612fcc9177d3318f12057ec11e92e616e70
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2023-07-07 17:58:23 +0300

    Added sorting to get_blogs helper functionr

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -20,9 +20,19 @@ if (!function_exists('get_blogs')) {
         $blog = null;
 
         if ($paginate) {
-            $blogs = $cat ? Article::where('article_category_id', $cat->id)->where('published', 1)->orderBy('created_at', 'DESC')->paginate($paginate) : [];
+            $blogs = $cat
+                ? Article::where('article_category_id', $cat->id)
+                ->where('published', 1)
+                ->orderBy('created_at', 'DESC')
+                ->paginate($paginate)
+                : [];
         } else {
-            $blogs = $cat ? Article::where('article_category_id', $cat->id)->where('published', 1)->orderBy('created_at', 'DESC')->get() : [];
+            $blogs = $cat
+                ? Article::where('article_category_id', $cat->id)
+                ->where('published', 1)
+                ->orderBy('created_at', 'DESC')
+                ->get()
+                : [];
         }
         return $blogs;
     }
